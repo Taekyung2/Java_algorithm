210ec61129be23883206972a6ab763f8076cf010
ltk3934 <ltk3934@daum.net>
2020-04-29 16:35:49 +0900

    ..

--- src/boj_algorithm/_10171_고양이.java
+++
@@ -1,7 +0,0 @@
-package boj_algorithm;
-
-public class _10171_고양이 {
-	public static void main(String[] args) {
-		System.out.printf("\\    /\\\n )  ( ')\n(  /  )\n \\(__)|");
-	}
-}
--- src/boj_algorithm/_10172_개.java
+++
@@ -1,7 +0,0 @@
-package boj_algorithm;
-
-public class _10172_개 {
-	public static void main(String[] args) {
-		System.out.printf("|\\_/|\n|q p|   /}\n( 0 )\"\"\"\\\n|\"^\"`    |\n||_/=\\\\__|");
-	}
-}
